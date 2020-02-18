4654daef17c86d44e7e59d54ed1ec00bad3b6409
khoablog <59403580+khoablog@users.noreply.github.com>
2020-02-18 14:10:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-style = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=style)
+app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
     html.H1(children="Hello Khoa!"),

e84070531599b5647777d2a7643e6a5cc0ce2b69
khoablog <59403580+khoablog@users.noreply.github.com>
2020-02-18 14:44:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from flask import Flask
 import os
-import cv2
 
 server = Flask(__name__)
 server.secret_key = os.environ.get('secret_key', 'secret')
